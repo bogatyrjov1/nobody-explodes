a20e65f9b206594f9f2d9b8150cb6019310e169f
Jevgeni Bogatyrjov <jevgeni@skyselect.com>
2018-06-25 04:18:28 +0300

    Add structure, remove roater, bootstrap components

---
+++ src/components/modules/memory/Memory.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="memory">
+        memory
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Memory"
+}
+</script>
+
+<style>
+.memory {
+    color: red;
+}
+</style>
